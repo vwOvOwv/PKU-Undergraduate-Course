eacf40e74ea7520d3f50fd3084acac8efe92b021
vwOvOwv <1271440868@qq.com>
2024-12-15 14:23:02 +0800

    delete

--- 计算机网络(Computer Networks)/Lab/lab1/test_local/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/workspace/code/2024-lab1-myftp-vwOvOwv-1/test_local
-# Build directory: /home/student/workspace/code/2024-lab1-myftp-vwOvOwv-1/test_local/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/student/workspace/code/2024-lab1-myftp-vwOvOwv-1/test_local/build/ftp_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- 计算机网络(Computer Networks)/Lab/lab1/test_local/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/workspace/code/2024-lab1-myftp-vwOvOwv-1/test_local/build/_deps/googletest-src/googletest
-# Build directory: /home/student/workspace/code/2024-lab1-myftp-vwOvOwv-1/test_local/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
