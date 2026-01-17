80738d089696cb3b4ab3b790a6817dd972080c03
vwOvOwv <1271440868@qq.com>
2026-01-17 12:15:16 +0800

    add Lv7

---
+++ 编译原理(Compiler Principles)/Lab/Lv7/sysy-make-template/src/main.cpp
@@ -0,0 +1,58 @@
+#include <cassert>
+#include <cstdio>
+#include <memory>
+#include <string>
+#include "ast.hpp"
+#include "asm.hpp"
+
+using namespace std;
+
+// 声明 lexer 的输入, 以及 parser 函数
+// 为什么不引用 sysy.tab.hpp 呢? 因为首先里面没有 yyin 的定义
+// 其次, 因为这个文件不是我们自己写的, 而是被 Bison 生成出来的
+// 你的代码编辑器/IDE 很可能找不到这个文件, 然后会给你报错 (虽然编译不会出错)
+// 看起来会很烦人, 于是干脆采用这种看起来 dirty 但实际很有效的手段
+extern FILE *yyin;
+extern int yyparse(unique_ptr<BaseAST> &ast);
+
+// std::map<std::string, SymbolInfo> global_symbol_table;
+SymbolTable symbol_table;
+
+int main(int argc, const char *argv[]) {
+	// 解析命令行参数. 测试脚本/评测平台要求你的编译器能接收如下参数:
+	// compiler 模式 输入文件 -o 输出文件
+	assert(argc == 5);
+	auto mode = argv[1];
+	auto input = argv[2];
+	auto output = argv[4];
+
+	if(string(output) != "stdout") {
+		freopen(output, "w", stdout);
+	}
+
+	// 打开输入文件, 并且指定 lexer 在解析的时候读取这个文件
+	yyin = fopen(input, "r");
+	assert(yyin);
+
+	// 调用 parser 函数, parser 函数会进一步调用 lexer 解析输入文件
+	unique_ptr<BaseAST> ast;
+	auto ret = yyparse(ast);
+	assert(!ret);
+
+	// 输出解析得到的 AST, 其实就是个字符串
+	// cout << *ast << endl;
+	// ast->dump();
+	// std::cout << std::endl;
+
+	// 根据 AST 生成 IR
+	auto program_ir = generate_ir(ast);
+	// 输出生成的 IR 或对应的 RISC-V 汇编
+	if (string(mode) == "-riscv") {
+		RiscvGenerator generator(program_ir.get());
+		generator.generate();
+	}
+	else{
+		program_ir->dump();
+	}
+	return 0;
+}
