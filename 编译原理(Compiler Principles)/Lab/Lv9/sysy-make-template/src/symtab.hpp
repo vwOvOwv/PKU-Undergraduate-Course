4e4896446563d3335d1dc0c51b619ad7aa6725c1
vwOvOwv <1271440868@qq.com>
2026-01-19 03:34:58 +0800

    update Lv9

--- 编译原理(Compiler Principles)/Lab/Lv9/sysy-make-template/src/symtab.hpp
+++ 编译原理(Compiler Principles)/Lab/Lv9/sysy-make-template/src/symtab.hpp
@@ -11,7 +11,6 @@ struct SymbolInfo {
     bool is_array = false;
     bool is_pointer = false;
     std::vector<int> dims;
-    std::vector<int> const_array_vals;
 };
 
 class SymbolTable {
@@ -34,9 +33,7 @@ public:
     }
 
     // 插入符号
-    std::string push(const std::string& id, bool is_const, int const_val, 
-        bool is_array = false, bool is_pointer = false, std::vector<int> dims = {},
-        std::vector<int> const_array_vals = {}) {
+    std::string push(const std::string& id, bool is_const, int const_val, bool is_array = false, bool is_pointer = false, std::vector<int> dims = {}) {
         // 全局变量保持原名
         // 局部变量使用 id + "_" + counter
         std::string unique_ir_name;
@@ -54,7 +51,7 @@ public:
         info.is_array = is_array;
         info.is_pointer = is_pointer;
         info.dims = dims;
-        info.const_array_vals = const_array_vals;
+
         scopes.back()[id] = info;
         return unique_ir_name;
     }
