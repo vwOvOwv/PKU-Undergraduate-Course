a3d63cfadb04eed682c8a0e1cff282bcca00abe4
vwOvOwv <1271440868@qq.com>
2026-01-20 17:15:59 +0800

    rename folders and files in EN

--- 生物信息学实验(Bioinformatics Lab.)/Week5/codes/chr21_genes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-anno_path="/gpfs1/share/class2/annotation.gtf"
-awk -F '\t' '$3=="gene"{print $0}' $anno_path > /home/teach80_pkuhpc/peiyuliu/genes.txt
-awk -F '\t' '$3=="gene"{print $9}' $anno_path | awk -F ' ' '{print $4}' > /home/teach80_pkuhpc/peiyuliu/gene_types.txt
-sort /home/teach80_pkuhpc/peiyuliu/gene_types.txt | uniq > uniq_gene_types.txt
--- 生物信息学实验(Bioinformatics Lab.)/Week5/codes/chr21_stats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-path="/gpfs1/share/class2/chr21.fa"
-total_length=$(wc -c $path | awk '{print $1}')
-A_counts=$(grep -io 'a' $path | wc -l)
-echo "The length of Chromosome21 is $total_length bp."
-echo "The counts of Adenine in Chromosome21 is $A_counts."
